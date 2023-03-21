Cat-CodeMaster/gateway

commit 175d29fb2cee9e38ef359c27ec43d31c259c9d72

Add dynamically setup containerPorts in proxy infrastructure (#1157)
* Add dynamically setup containerPorts in proxy infrastructure

Signed-off-by: huxiaohan <shawnhxh@outlook.com>

* Remove old port config and add some test cases

Signed-off-by: huxiaohan <shawnhxh@outlook.com>

* Add a separate test for container port

Signed-off-by: Shawnh2 <shawnhxh@outlook.com>

---------

Signed-off-by: huxiaohan <shawnhxh@outlook.com>
Signed-off-by: Shawnh2 <shawnhxh@outlook.com>
Co-authored-by: Xunzhuo <bitliu@tencent.com>

internal/infrastructure/kubernetes/proxy_deployment.go
@@ -127,17 +127,16 @@ func (i *Infra) expectedProxyDeployment(infra *ir.Infra) (*appsv1.Deployment, er
 }
 
 func expectedProxyContainers(infra *ir.Infra) ([]corev1.Container, error) {
-	ports := []corev1.ContainerPort{
-		{
-			Name:          "http",
-			ContainerPort: envoyHTTPPort,
-			Protocol:      corev1.ProtocolTCP,
-		},
-		{
-			Name:          "https",
-			ContainerPort: envoyHTTPSPort,
-			Protocol:      corev1.ProtocolTCP,
-		},
+	var ports []corev1.ContainerPort
+	for _, listener := range infra.Proxy.Listeners {
+		for _, p := range listener.Ports {
+			port := corev1.ContainerPort{
+				Name:          p.Name,
+				ContainerPort: p.ContainerPort,
+				Protocol:      corev1.Protocol(p.Protocol),
+			}
+			ports = append(ports, port)
+		}
 	}
 
 	var cfg string

internal/infrastructure/kubernetes/proxy_deployment_test.go
@@ -127,6 +127,22 @@ func TestExpectedProxyDeployment(t *testing.T) {
 
 	infra.Proxy.GetProxyMetadata().Labels[gatewayapi.OwningGatewayNamespaceLabel] = "default"
 	infra.Proxy.GetProxyMetadata().Labels[gatewayapi.OwningGatewayNameLabel] = infra.Proxy.Name
+	infra.Proxy.Listeners = []ir.ProxyListener{
+		{
+			Ports: []ir.ListenerPort{
+				{
+					Name:          "EnvoyHTTPPort",
+					Protocol:      ir.TCPProtocolType,
+					ContainerPort: envoyHTTPPort,
+				},
+				{
+					Name:          "EnvoyHTTPSPort",
+					Protocol:      ir.TCPProtocolType,
+					ContainerPort: envoyHTTPSPort,
+				},
+			},
+		},
+	}
 
 	deploy, err := kube.expectedProxyDeployment(infra)
 	require.NoError(t, err)
@@ -231,6 +247,46 @@ func TestExpectedPodAnnotations(t *testing.T) {
 	deploy, err := kube.expectedProxyDeployment(infra)
 	require.NoError(t, err)
 	checkPodAnnotations(t, deploy, annotations)
+}
+
+func TestExpectedContainerPort(t *testing.T) {
+	const FooContainerPort, BarContainerPort = 7878, 8989
+
+	svrCfg, err := config.New()
+	require.NoError(t, err)
+	cli := fakeclient.NewClientBuilder().WithScheme(envoygateway.GetScheme()).WithObjects().Build()
+	kube := NewInfra(cli, svrCfg)
+	infra := ir.NewInfra()
+
+	infra.Proxy.GetProxyMetadata().Labels[gatewayapi.OwningGatewayNamespaceLabel] = "default"
+	infra.Proxy.GetProxyMetadata().Labels[gatewayapi.OwningGatewayNameLabel] = infra.Proxy.Name
+	infra.Proxy.Listeners = []ir.ProxyListener{
+		{
+			Ports: []ir.ListenerPort{
+				{
+					Name:          "FooPort",
+					Protocol:      ir.TCPProtocolType,
+					ContainerPort: FooContainerPort,
+				},
+			},
+		},
+		{
+			Ports: []ir.ListenerPort{
+				{
+					Name:          "BarPort",
+					Protocol:      ir.UDPProtocolType,
+					ContainerPort: BarContainerPort,
+				},
+			},
+		},
+	}
+
+	deploy, err := kube.expectedProxyDeployment(infra)
+	require.NoError(t, err)
+	ports := []int32{FooContainerPort, BarContainerPort}
+	for _, port := range ports {
+		checkContainerHasPort(t, deploy, port)
+	}
 }
 
 func deploymentWithImage(deploy *appsv1.Deployment, image string) *appsv1.Deployment {

commit c09707df0b121f19190ed55533ce00060c9d2619

Fix `invalid: protocol: Unsupported value: "HTTP"` error (#1200)
Fixes this error
```
2023-03-22T01:02:38.760+0700    ERROR   runner/runner.go:68     failed to create new infra      {"runner": "infrastructure", "error": "failed to update deployment envoy-gateway-system/envoy-qat-infra-shared-gw-7161742d: Deployment.apps \"envoy-qat-infra-shared-gw-7161742d\" is invalid: spec.template.spec.containers[0].ports[0].protocol: Unsupported value: \"HTTP\": supported values: \"SCTP\", \"TCP\", \"UDP\""} 
```

Caused by https://github.com/envoyproxy/gateway/pull/1157

Fixes: https://github.com/envoyproxy/gateway/issues/1198

Signed-off-by: Quyền Hoàng <120354221+quyenhoang96@users.noreply.github.com>

internal/infrastructure/kubernetes/proxy_deployment.go
@@ -127,13 +127,25 @@ func (i *Infra) expectedProxyDeployment(infra *ir.Infra) (*appsv1.Deployment, er
 }
 
 func expectedProxyContainers(infra *ir.Infra) ([]corev1.Container, error) {
+	// Define slice to hold container ports
 	var ports []corev1.ContainerPort
+
+	// Iterate over listeners and ports to get container ports
 	for _, listener := range infra.Proxy.Listeners {
 		for _, p := range listener.Ports {
+			var protocol corev1.Protocol
+			switch p.Protocol {
+			case ir.HTTPProtocolType, ir.HTTPSProtocolType, ir.TLSProtocolType, ir.TCPProtocolType:
+				protocol = corev1.ProtocolTCP
+			case ir.UDPProtocolType:
+				protocol = corev1.ProtocolUDP
+			default:
+				return nil, fmt.Errorf("invalid protocol %q", p.Protocol)
+			}
 			port := corev1.ContainerPort{
 				Name:          p.Name,
 				ContainerPort: p.ContainerPort,
-				Protocol:      corev1.Protocol(p.Protocol),
+				Protocol:      protocol,
 			}
 			ports = append(ports, port)
 		}